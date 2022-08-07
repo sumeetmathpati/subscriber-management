41dc7cc0a19dbd354fc85da529842a3d2440ba5b
sumeetmathpati <sumeet.mathpati@gmail.com>
2022-08-07 12:09:17 +0530

    Included user_router.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
 from routers.app import router as app_router
+from routers.user import router as user_router
 
 app =FastAPI()
 app.include_router(app_router)
+app.include_router(user_router)
 
 @app.get("/")
 def root():
