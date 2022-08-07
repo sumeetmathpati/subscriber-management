ac329696d78e7f96dd4b4256ccfd81c8f154852d
sumeetmathpati <sumeet.mathpati@gmail.com>
2022-08-07 11:59:26 +0530

    Added field id in App of type PyObjectId.

--- models/app.py
+++ models/app.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from bson import ObjectId
 
+from .utils import PyObjectId
 
 class App(BaseModel):
+    id: PyObjectId = Field(alias="_id")
     name: str = None
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
